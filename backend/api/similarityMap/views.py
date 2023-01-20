778a657b94b54bf0c69961407becefd4092de178
asmabatooll <123125900+asmabatooll@users.noreply.github.com>
2023-01-20 21:13:51 +0000

    similarityMap API changes

--- backend/api/similarityMap/views.py
+++ backend/api/similarityMap/views.py
@@ -1,46 +1,55 @@
 from django.http import HttpResponse
+from api.similarityMap.serializers import SimilarityRequestSerializer
 from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem.Draw import SimilarityMaps
-from IPython.display import SVG
 import io
 from PIL import Image
 import numpy as np
 import rdkit
+from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 
 
 class SimilarityMap(APIView):
     @extend_schema(
         parameters=[
             OpenApiParameter(
-                name="smiles",
+                name="smiles1",
                 style="query",
             ),
             OpenApiParameter(
-                name="smiles",
+                name="smiles2",
                 style="query",
             ),
         ],
         responses={
             (200, "*/*"): OpenApiTypes.BYTE,
-            (200, "applications/json"): OpenApiTypes.BYTE,
+            (200, "image/svg+xml"): OpenApiTypes.BYTE,
         },
     )
-    def show_png(data):
-        bio = io.BytesIO(data)
-        img = Image.open(bio)
-        return img
-
     def get(self, request):
+        txt = request.query_params
+        print("******************")
+        print(txt)
 
-        data = request.query_params
-        data.is_valid(raise_exception=True)
-        smiles1 = data[0].validated_data["smiles"]
-        smiles2 = data[1].validated_data["smiles"]
+        serializer1 = SimilarityRequestSerializer(data=txt)
+        # serializer2 = SimilarityRequestSerializer(data=txt["smiles2"])
+
+        serializer1.is_valid(raise_exception=True)
+        # serializer2.is_valid(raise_exception=True)
+
+        smiles1 = serializer1.validated_data["smiles1"]
+        smiles2 = serializer1.validated_data["smiles2"]
 
         mol1 = Chem.MolFromSmiles(smiles1)
         mol2 = Chem.MolFromSmiles(smiles2)
+        img = Draw.MolsToGridImage((smiles1, smiles2))
 
+        return HttpResponse(img, content_type="image/svg+xml")
+
+        """
         d = Draw.MolDraw2DCairo(400, 400)
         _, maxWeight = SimilarityMaps.GetSimilarityMapForFingerprint(
             mol1,
@@ -50,7 +59,10 @@ class SimilarityMap(APIView):
             ),
             draw2d=d,
         )
-        d.FinishDrawing()
-        image = self.show_png(d.GetDrawingText())
 
-        return HttpResponse(image, content_type="image/png")
+        d.FinishDrawing()
+        bio = io.BytesIO(d.GetDrawingText())
+        img = Image.open(bio)
+
+        return HttpResponse(img, content_type="image/png")
+        """
